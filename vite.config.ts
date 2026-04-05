645f5c209c5346d9fd4b4e9fb35f3d1a031df494
ek-mc <ek-mc@users.noreply.github.com>
2026-04-05 15:12:27 +0300

    configure pages for topografiko

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/openkaek/",
+  base: "/topografiko/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
